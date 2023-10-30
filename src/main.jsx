be754f4140fe47db547a86f69d4d5e70b7b5499f
Jason Githui <jasongithui@gmail.com>
2023-10-30 13:52:14 +0300

    fix hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
